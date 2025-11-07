d32b9aa04712c584333440416a7cf8e870cb6dce
daniel ellm <ellm28@gmail.com>
2025-11-07 13:53:33 +0000

    leider hat sich jetzt der schwarz verlauf unten beim header cover wieder

--- src/components/film-page-client.tsx
+++ src/components/film-page-client.tsx
@@ -185,7 +185,7 @@ export default function FilmPageClient({ films: unsortedFilms, initialSlug }: Fi
           </div>
         </div>
         
-        <div className="absolute inset-0 z-10 flex flex-col justify-center items-start bg-gradient-to-b from-black/0 via-black/30 to-black/95 pointer-events-none">
+        <div className="absolute inset-0 z-10 flex flex-col justify-center items-start bg-gradient-to-t from-black via-black/50 to-transparent pointer-events-none">
           <div className="w-full max-w-screen-2xl mx-auto px-4 md:px-6 relative h-full flex flex-col justify-end pb-24 md:pb-32 items-start">
             
             <div className="w-full">
